6ece2f9a8f47d71dabfa3c3f115e1ef95e243e60
유준영 <__@.com>
2025-12-02 15:41:36 +0900

    ...

--- project1/quiz.js
+++ project1/quiz.js
@@ -39,29 +39,29 @@ const quiz = [
     {
         q: "5) 오늘은 쇼핑하는 날! 내가 사고싶은 옷은??",
         options: [
-            { label: "스투시, 슈프림, 스톤아일랜드", img: "img/street.jpg", score: 1 },
+            { label: "스투시, 슈프림, 스톤아일랜드", img: "img/street.png", score: 1 },
             { label: "헤지스, 빈폴, 닥스",           img: "img/logo.jpg",       score: 0 }
         ]
     },
     {
         q: "6) 젊은 직원이 어려움에 처했을때 나의 행동은??",
         options: [
-            { label: "힘들지? 그거.. 다 그런거야.", img: "img/tough-times.png", score: 1 },
-            { label: "와.. 이거 너무 스트레스네. 우리 일단 커피 한 잔 할까?", img: "img/coffee.png", score: 0 }
+            { label: "힘들지? 그거.. 다 그런거야.", img: "img/todak.jpg", score: 1 },
+            { label: "와.. 이거 너무 스트레스네. 우리 일단 커피 한 잔 할까?", img: "img/coffee.jpg", score: 0 }
         ]
     },
     {
         q: "7) 물놀이를 간다면 더 먹고 싶은것은??",
         options: [
-            { label: "한방 능이 백숙", img: "img/hanbang-neungi-baeksuk.png", score: 0 },
-            { label: "바베큐",         img: "img/barbecue.png",              score: 1 }
+            { label: "한방 능이 백숙", img: "img/chicken.jpg", score: 0 },
+            { label: "바베큐",         img: "img/bbq.jpg",              score: 1 }
         ]
     },
     {
         q: "8) 더 편한 SNS소통 방식은??",
         options: [
-            { label: "인스타그램 DM", img: "img/instagram-dm.png", score: 1 },
-            { label: "카카오톡",      img: "img/kakaotalk.png",    score: 0 }
+            { label: "인스타그램 DM", img: "img/insta.jpg", score: 1 },
+            { label: "카카오톡",      img: "img/kakao.jpg",    score: 0 }
         ]
     },
 ];
